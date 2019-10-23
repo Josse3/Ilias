2be12329666c514f12fc498a4b2f7d92451bd720
Josse3 <josse.cottenier@hotmail.com>
2019-10-23 17:31:21 +0200

    Added Odyssey

---
+++ client/src/views/Odyssee/Odyssee.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import '../../styles/ErrorModal.css';
+
+import Toolbar from '../../components/Toolbar/Toolbar';
+import Reader from '../../components/Reader/Reader';
+
+import FetchText from '../../util/FetchText';
+
+const Odyssee = () => {
+    const [userInput, setUserInput] = useState({
+        start: '1.1',
+        amount: 10
+    }); // User input, to be received from Toolbar component
+    const [visualPreference, setVisualPreference] = useState({}); // User's visual preference, to be received from Toolbar component
+
+
+    const [errorModalText, setErrorModalText] = useState('');
+    const [loading, setLoading] = useState(false); // Boolean defining whether or not loading screen is being shown
+
+    const [fetchedText, setFetchedText] = useState('');
+
+    const updateUserInput = details => setUserInput({ ...userInput, [details.key]: details.value });
+
+    const retrieveText = async () => {
+        // Validate input
+        const verseAdressRegexp = /\b[0-9]\.[0-9]{1,3}\b/; // Matches a verse adress, e.g. 3.1 or 6.325
+        if (!verseAdressRegexp.test(userInput.start)) {
+            return setErrorModalText('Het beginvers dat u heeft ingevoerd is ongeldig. Probeer alstublieft opnieuw.');
+        };
+
+        if (userInput.amount >= 30) {
+            return setErrorModalText('Door beperkingen van de Perseus-database is het opvragen van 30 verzen of meer niet mogelijk.');
+        }
+
+        FetchText.metric({
+            start: userInput.start,
+            amount: userInput.amount,
+            path: 'odyssee',
+            setLoading,
+            setFetchedText
+        });
+    }
+
+    useEffect(() => {
+        if (fetchedText) console.log(fetchedText);
+    }, [fetchedText]);
+
+    return (
+        <div className="odyssee">
+            <Toolbar
+                handleTextFetch={retrieveText}
+                updateVisualPreference={setVisualPreference}
+                updateUserInput={updateUserInput}
+            />
+            <Reader
+                visualPreference={visualPreference}
+                handleTextFetch={retrieveText}
+                loading={loading}
+                text={fetchedText}
+            />
+            {errorModalText && <div className="error-modal">
+                {errorModalText}
+                <button onClick={() => setErrorModalText('')}>OK</button>
+            </div>}
+        </div>
+    );
+}
+
+export default Odyssee;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 const router = express.Router();
 
-router.get('/perseus', (req, res) => {
+router.get('/ilias', (req, res) => {
     const start = decodeURIComponent(req.query.start);
     const end = decodeURIComponent(req.query.end);
     fetch(`https://scaife.perseus.org/library/passage/urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:${start}-${end}/xml/`)
@@ -14,6 +14,17 @@ router.get('/perseus', (req, res) => {
         })
         .then(textResponse => res.send(textResponse));
 });
+
+router.get('/odyssee', (req, res) => {
+    const start = decodeURIComponent(req.query.start);
+    const end = decodeURIComponent(req.query.end);
+    fetch(`https://scaife.perseus.org/library/passage/urn:cts:greekLit:tlg0012.tlg002.perseus-grc2:${start}-${end}/xml/`)
+        .then(response => {
+            if (!response.ok) throw Error('Failed fetching text from Perseus.');
+            return response.text();
+        })
+        .then(textResponse => res.send(textResponse));
+})
 
 app.use('/grieks', router);
 
