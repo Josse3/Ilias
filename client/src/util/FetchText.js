6999ebee34c3438f8e824778329b49605bd44393
Josse3 <josse.cottenier@hotmail.com>
2019-10-23 17:30:08 +0200

    Compatibality with Odyssey

--- client/src/util/FetchText.js
+++ client/src/util/FetchText.js
@@ -1,5 +1,5 @@
 const FetchText = {
-    metric({ start, amount, setLoading, setFetchedText }) {
+    metric({ start, amount, path, setLoading, setFetchedText }) {
         // Get last verse
         const firstVerseNumber = Number(start.split('.')[1]); // Extracts verse line from user input as a number
         const lastVerseNumber = firstVerseNumber + Number(amount); // Forms last verse as a number
@@ -9,16 +9,24 @@ const FetchText = {
         setLoading(true);
         start = encodeURIComponent(start);
         const end = encodeURIComponent(lastVerse);
-        fetch(`/grieks/perseus?start=${start}&end=${end}`)
+        fetch(`/grieks/${path}?start=${start}&end=${end}`)
             .then(response => {
                 if (!response.ok) throw Error('Failed fetching text from Perseus.');
                 return response.text();
             })
             .then(textResponse => {
                 setLoading(false);
-                const relevantXML = textResponse.split(/<\/?div(?: type="textpart" subtype="Book" n="[0-9][0-9]?")?>/)[1]; // Extracting XML body via RegExp
+                let relevantXML;
+
+                if (path === 'ilias') {
+                    relevantXML = textResponse.split(/<\/?div(?: type="textpart" subtype="Book" n="[0-9][0-9]?")?>/)[1]; // Extracting XML body via RegExp
+                } else if (path === 'odyssee') {
+                    relevantXML = textResponse.split(/<\/?div(?: n="[0-9]" type="textpart" subtype="book")?>/)[1];
+                }
+
                 const versesXML = relevantXML
                     .replace(/<\/?q>/g, '') // Delete <q> and </q> elements
+                    .replace(/<q type="unspecified">/, '') // Delete <q type="unspecified">-elements
                     .replace(/<milestone.{1,40}\/>/g, ''); // Delete <milestone> tags
                 // Parse received XML into object
                 const versesObject = {};
