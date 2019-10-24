a5a8f6f66c1adf2f9d6b2f813d57f0a4abde3088
Josse3 <josse.cottenier@hotmail.com>
2019-10-24 17:37:17 +0200

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Index from './views/Index/Index';
 import Ilias from './views/Ilias/Ilias';
+import Odyssee from './views/Odyssee/Odyssee';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/ilias" exact component={Ilias} />
+        <Switch>
+          <Route path="/" exact component={Index} />
+          <Route path="/ilias" exact component={Ilias} />
+          <Route path="/odyssee" exact component={Odyssee} />
+        </Switch>
       </Router>
     </div>
   );
