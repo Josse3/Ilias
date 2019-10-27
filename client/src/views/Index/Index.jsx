8f7db3611c49e2c262177afa878e0637194b3d51
Josse3 <josse.cottenier@hotmail.com>
2019-10-27 11:53:13 +0100

    Removed big header, added footer

--- client/src/views/Index/Index.jsx
+++ client/src/views/Index/Index.jsx
@@ -19,13 +19,11 @@ const Index = () => {
 
     return (
         <div className="index">
-            <h1>Grieks online</h1>
-            <p>Door Josse Cottenier</p>
-            <h2>5de jaar</h2>
+            <h1>5de jaar</h1>
             <div className="tiles-container">
                 {content.fifthYear.map(obj => {
                     return (
-                        <Link to={obj.path}>
+                        <Link to={obj.path} key={obj.path}>
                             <div className="tile" key={obj.path}>
                                 <h3>{`${obj.author}, ${obj.path}`}</h3>
                             </div>
@@ -33,6 +31,9 @@ const Index = () => {
                     );
                 })}
             </div>
+            <footer>
+                Website gemaakt door <a href="https://jossecottenier.tech" target="_blank" rel="noopener noreferrer">Josse Cottenier</a> | Tekstinhoud door <a href="https://scaife.perseus.org" target="_blank" rel="noopener noreferrer">Perseus</a>
+            </footer>
         </div>
     )
 }
